8b821638c1038d7bf1a6595ec7e63380c839f3d6
w13522360282 <ljj_48@126.com>
2018-04-02 18:07:56 +0800

    first commit

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,14 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  let result = [];
+  for(let i = 0 ; i < collection.length ; i ++){
+    if(collection[i] % 2 == 0 ){
+      result.push(collection[i]);
+    }
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports = collect_all_even;
--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,14 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  let result = [];
+  for(let i = 0 ; i < collection_b.length ; i ++){
+    if(collection_a.includes(collection_b[i])){
+      result.push(collection_b[i]);
+    }
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports = get_intersection;
