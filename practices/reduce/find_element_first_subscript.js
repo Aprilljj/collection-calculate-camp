c0c004758e22d7960bcdd06499da05bfc7d4a208
twcn <twcn@twcns-MacBook-Pro-2.local>
2018-04-15 21:54:10 +0800

    周末的战果

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,6 +2,14 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  let result = [];
+  for(let i = 0 ; i < collection.length ; i ++){
+    if(collection[i] == element){
+      result.push(i);
+    }
+  }
+  console.log(result[0]);
+  return result[0];
 }
 
 module.exports = calculate_elements_sum;
--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -2,6 +2,14 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  let result = [];
+  for(let i = 0 ; i < collection.length ; i ++){
+    if(collection[i] == element){
+      result.push(i);
+    }
+  }
+  console.log(result[result.length - 1]);
+  return result[result.length - 1];
 }
 
 module.exports = calculate_elements_sum;
