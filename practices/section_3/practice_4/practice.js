a9e6ea289f01a6206519d3983523295f1869f532
twcn <twcn@twcns-MacBook-Pro-2.local>
2018-04-24 00:09:02 +0800

    4月23日完成

--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,38 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  let create_updated_collection = [];
+  let result = {};
+  let collection_b = object_b[ Object.keys(object_b) ];
+  for(var i in collection_a){
+    if(collection_a[i].includes('-')){
+      let temp = collection_a[i].split('-');
+      for(let i = 0 ; i < temp[1] ; i ++){
+        collection_a.push(temp[0]);
+      }
+      collection_a.splice(i,1);
+    }
+  }
+  for(let i = 0 ; i < collection_a.length ; i ++){
+    if(!result[collection_a[i]]){
+      result[collection_a[i]] = 1 ;
+    }else{
+      result[collection_a[i]] ++;
+    }
+  }
+  for(let i = 0 ; i < Object.keys(result).length ; i ++){
+    if(collection_b.includes(Object.keys(result)[i])){
+      result[Object.keys(result)[i]] = result[Object.keys(result)[i]] - Math.floor(result[Object.keys(result)[i]] / 3); 
+    }
+  }
+  
+  for(var i in Object.keys(result)){
+    let temp = {};
+    temp.key = Object.keys(result)[i];
+    temp.count = result[Object.keys(result)[i]];
+    create_updated_collection.push(temp);
+  }
+  console.log(create_updated_collection);
+  return create_updated_collection;
 }
 
 module.exports = create_updated_collection;
--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,26 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
   //这里写代码。。。
+  let new_collection = collection.sort(function(a,b){return a-b;});
+  let result = [];
+  for(let i = 0 ; i < new_collection.length ; i+= 3){
+    result.push(new_collection.slice(i , i +3));
+  }
+  let rank_by_two_large_one_small = [];
+  for(let i = 0 ; i < result.length ; i ++){
+    if(result[i].length % 3 == 0){
+      var a = result[i].shift();
+      result[i].push(a);
+      for(let j = 0 ; j < result[i].length ; j ++){
+        rank_by_two_large_one_small.push(result[i][j]);
+      }
+    }else{
+      for(var j in result[i]){
+        rank_by_two_large_one_small.push(result[i][j]);
+      }
+    }
+  }
+  console.log(rank_by_two_large_one_small);
+  return rank_by_two_large_one_small;
 }
 module.exports = rank_by_two_large_one_small;
