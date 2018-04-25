96506d48896a31872936df2ad8b729c3bd9fb254
w13522360282 <ljj_48@126.com>
2018-04-25 09:55:10 +0800

    4月25日完成最后一题

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,13 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let spilt_to_zero = [];
+  let count = Math.ceil(number / interval);
+  for(let i = 0 ; i <= count  ; i ++){
+    spilt_to_zero.push(parseFloat((number - interval * i).toFixed(1)));
+  }  
+  console.log(spilt_to_zero);
+  return spilt_to_zero;
 }
 
 module.exports = spilt_to_zero;
